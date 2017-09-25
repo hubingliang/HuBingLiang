79169e403ebb41fee95d9054bc3bfe78fb2bb3c4
brown <1104524352@qq.com>
2017-09-25 13:05:17 +0800

    粒子效果完成

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,20 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <hello></hello>
+    <Particles></Particles>
+    <div class="s">sds</div>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
+import Particles from './components/Particles'
 
 export default {
   name: 'app',
   components: {
-    Hello
+    Particles
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
