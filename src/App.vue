4de24bafc186a8376a8ba3e80fdda863a3cdfe6b
brown <1104524352@qq.com>
2017-09-27 22:23:55 +0800

    项目组件完成

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,129 @@
 <template>
   <div id="app" >
     <nav class="animated fadeInRight">
-      <div class="nav-item" @click="particles = false,unparticles = true" ></div>
-      <div class="nav-item" @click="particles = true,unparticles = false"></div>
-      <div class="nav-item"></div>
-      <div class="nav-item"></div>
-      <div class="nav-item"></div>
-      <div class="nav-item"></div>
-      <div class="nav-item"></div>
+      <div class="nav-item" v-bind:class="{active: ParticlesShow}" @click="ParticlesShow = true,confessionShow = true,todoShow = true,resumeShow = true,musicShow = true,clockShow=true"></div>
+      <div class="nav-item" @click="confessionShow = true,ParticlesShow = false,todoShow = true,resumeShow = true,musicShow = true,clockShow=true"></div>
+      <div class="nav-item" @click="confessionShow = true,ParticlesShow = false,todoShow = true,resumeShow = false,musicShow = true,clockShow=true"></div>
+      <div class="nav-item" @click="confessionShow = true,ParticlesShow = false,todoShow= false,resumeShow = false,musicShow = true,clockShow=true"></div>
+      <div class="nav-item" @click="confessionShow = true,ParticlesShow = false,todoShow= false,resumeShow = false,musicShow = false,clockShow=true"></div>
+      <div class="nav-item" @click="confessionShow = false,ParticlesShow = false,todoShow= false,resumeShow = false,musicShow = false,clockShow = true"></div>
       <div class="nav-item"></div>
     </nav>
-    <Particles v-bind:class="{move : particles,moveback : unparticles}"></Particles>
-    <Confession v-bind:class="{move : confession,moveback : unconfession}"></Confession>
+
+    <transition
+      name="custom-classes-transition"
+      enter-active-class="animated slideInDown"
+      leave-active-class="animated slideOutUp">
+      <Particles v-show="ParticlesShow"></Particles>
+    </transition>
+
+    <transition
+      name="custom-classes-transition"
+      enter-active-class="animated slideInDown"
+      leave-active-class="animated slideOutUp">
+      <Project v-show="resumeShow" v-bind:project="resume"></Project>
+    </transition>
+
+    <transition
+      name="custom-classes-transition"
+      enter-active-class="animated slideInDown"
+      leave-active-class="animated slideOutUp">
+      <Project v-show="todoShow" v-bind:project="todo"></Project>
+    </transition>
+
+    <transition
+      name="custom-classes-transition"
+      enter-active-class="animated slideInDown"
+      leave-active-class="animated slideOutUp">
+      <Project v-show="musicShow" v-bind:project="music"></Project>
+    </transition>
+
+    <transition
+      name="custom-classes-transition"
+      enter-active-class="animated slideInDown"
+      leave-active-class="animated slideOutUp">
+      <Project v-show="confessionShow" v-bind:project="confession"></Project>
+    </transition>
+
+    <transition
+      name="custom-classes-transition"
+      enter-active-class="animated slideInDown"
+      leave-active-class="animated slideOutUp">
+      <Project v-show="clockShow" v-bind:project="clock"></Project>
+    </transition>
+
+    
+
+    
+
+    
+    
   </div>
 </template>
 
 <script>
 import Particles from './components/Particles'
-import Confession from './components/Confession'
+import Project from './components/Project'
 
 export default {
   data(){
     return{
-      particles: false,
-      confession: false,
-      unparticles: false,
-      unconfession: false,
+      ParticlesShow:true,
+      confessionShow:true,
+      todoShow:true,
+      resumeShow:true,
+      musicShow:true,
+      clockShow:true,
+      confession:{
+        class:'confession',
+        name:'天农，表白，墙',
+        technologyStack:'Vue ES6 Webpack Less',
+        description:'一款基于LeanCloud为后台的表白墙网站，以Vue为框架，可登陆和注册，并提供发帖（图片上传）、评论、点赞等功能。',
+        url:'https://hubingliang.github.io/Confession-wall/dist/',
+        computer:'../static/computer.png',
+        page:'../static/confession.png'
+      },
+      todo:{
+        class:'todo',
+        name:'待办事项应用',
+        technologyStack:'Vue ES6 Webpack Less',
+        description:'一款基于LeanCloud为后台的表白墙网站，以Vue为框架，可登陆和注册，并提供发帖（图片上传）、评论、点赞等功能。',
+        url:'https://hubingliang.github.io/ToDo/dist/',
+        computer:'../static/computer.png',
+        page:'../static/todo.png'
+      },
+      resume:{
+        class:'resume',
+        name:'在线简历生成器',
+        technologyStack:'Vue ES6 Webpack Less',
+        description:'一款基于LeanCloud为后台的表白墙网站，以Vue为框架，可登陆和注册，并提供发帖（图片上传）、评论、点赞等功能。',
+        url:'https://hubingliang.github.io/vue-resumer/dist/',
+        computer:'../static/computer.png',
+        page:'../static/resume.png'
+      },
+      music:{
+        class:'music',
+        name:'手机端网易云',
+        technologyStack:'Vue ES6 Webpack Less',
+        description:'一款基于LeanCloud为后台的表白墙网站，以Vue为框架，可登陆和注册，并提供发帖（图片上传）、评论、点赞等功能。',
+        url:'https://hubingliang.github.io/NetEase-cloud-music/dist/',
+        computer:'../static/iphone.png',
+        page:'../static/music.png'
+      },
+      clock:{
+        class:'clock',
+        name:'原生JS实现时钟',
+        technologyStack:'Vue ES6 Webpack Less',
+        description:'一款基于LeanCloud为后台的表白墙网站，以Vue为框架，可登陆和注册，并提供发帖（图片上传）、评论、点赞等功能。',
+        url:'https://hubingliang.github.io/project/clock/clock.html',
+        computer:'../static/computer.png',
+        page:'../static/clock.png'
+      },
     }
   },
   name: 'app',
   components: {
-    Particles,Confession
+    Particles,Project
   }
 }
 </script>
---
+++ src/components/Project.vue
@@ -0,0 +1,33 @@
+<template>
+  <div v-bind:class="project.class">
+      <img v-bind:src="project.page"  class="page" v-bind:class="{moveleft:moveleft,moveright:moveright}"/>
+      <img v-bind:src="project.computer"  class="computer" v-bind:class="{moveleft:moveleft,moveright:moveright}"/>
+      <div class="mask">
+          <div class="container">
+            <h2 class="animated fadeInLeft">{{project.name}}</h2>
+            <p class="frame animated slideInLeft">{{project.technologyStack}}</p>
+            <p class="description animated slideInLeft">
+                {{project.description}}
+            </p>
+            <button class="animated slideInLeft" v-on:mouseenter="moveleft = true,moveright = false"  v-on:mouseleave="moveleft = false,moveright = true" @click="jump">VIEW PROJECT</button>
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            moveleft: false,
+            moveright: false
+        }
+    },
+    props:['project'],
+    methods:{
+      jump:function(){
+          window.open(`${this.project.url}`)
+      }
+  }
+}
+</script>
