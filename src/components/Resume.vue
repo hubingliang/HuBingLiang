1b81ef6ea4a5bd6929d8b22fbd8ad4c55b30f358
brown <1104524352@qq.com>
2017-09-28 17:02:25 +0800

    css前缀

---
+++ src/components/Resume.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="table">
+      <div class="resume">
+          <div class="name">胡秉亮</div>
+          <div class="job">前端工程师</div>
+          <img src="" alt="">
+          <div class="left">
+              <h4></h4>
+          </div>
+      </div>
+  </div>
+</template>
